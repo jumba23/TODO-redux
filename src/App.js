73f348ad9c24f30376857c07df0375a814148180
Goran SD <g.cvetic23@gamail.com>
2022-12-07 11:02:07 -0800

    styling added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+       <h2>
+       Todo List App in REDUX
+       </h2>
+       <form>
+        <input
+        placeholder='Enter a Todo'
+        style={{width:350,padding:10, borderRadius:20, border:"none", fontSize:20}}
         >
-          Learn React
-        </a>
+        </input>
+        <button type="submit" style={{padding:12, borderRadius: 25, fontSize:15, marginLeft:20}}>Go</button>
+       </form>
+       <ul className='allTodos'>
+        <li className='singleTodo'>
+          <span className='todoText'>
+            First Todo
+          </span>
+          <button style={{borderRadius: 25, padding:10, border:"1px solid white", color: "white", backgroundColor:"orange"}}>Delete</button>
+        </li>
+       </ul>
       </header>
     </div>
   );
