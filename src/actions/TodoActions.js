426fcd485acb7ec18fbc3a9317634b8438137261
Goran SD <g.cvetic23@gamail.com>
2022-12-07 11:16:15 -0800

    adding reducer and store

---
+++ src/actions/TodoActions.js
@@ -0,0 +1,14 @@
+export const AddTodoAction = (todo) => (dispatch, getState) => {
+    const {
+        Todo: {todos},
+    } = getState()
+
+    const hasTodo=todos.find(i=>i.todo === todo)
+
+    if(!hasTodo && todo !== '') {
+        dispatch({
+            type:"ADD_TODO_SUCCESS",
+            payload: [ {id:todo, todo}, ...todos],
+        })
+    }
+}
